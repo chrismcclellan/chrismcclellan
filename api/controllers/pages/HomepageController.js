719729db2a68f510df4972aa2b05ec9c072b1d31
Christopher McClellan <chris_mcclellan@Christophers-MacBook-Pro.local>
2016-02-16 08:09:06 -0500

    inline css

--- api/controllers/pages/HomepageController.js
+++ api/controllers/pages/HomepageController.js
@@ -5,7 +5,8 @@
  * @description :: Server-side logic for managing series
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var fs = require('fs');
+var path = require('path');
 var async = require('async');
 var moment = require('moment');
 
@@ -26,8 +27,50 @@ module.exports = {
             }
         },
         function(error, data) {
+
             if (error) return res.error(500);
-            res.view('pages/homepage/page', data);
+
+            sails.hooks.views.render('pages/homepage/page', data, function(error, html) {
+
+                if (error) return res.serverError(error);
+
+                console.log('rendered html');
+
+                // get css
+                fs.readFile(path.join(__dirname, '../../../assets/css/app.css'), "utf8", function(err, css) {
+
+                    if (err) throw err;
+
+                    // console.log('get css', css);
+
+                    // inline css
+                    html = html.replace('<link rel="stylesheet" type="text/css" href="/css/app.css" />', '<style>' + css + '</style>');
+
+                    // minify
+                    var minified = require('html-minifier').minify(html, {
+                        removeComments: true,
+                        removeTagWhitespace: true,
+                        removeEmptyAttributes: true,
+                        caseSensitive: true,
+                        removeAttributeQuotes: true
+                    }, function() {
+                        console.log('callback');
+                    });
+
+                    // console.log('minify', minfied);
+
+                    // write to index.html
+                    fs.writeFile(path.join(__dirname, '../../../index.html'), minified, 'utf8', function (err) {
+
+                        if (err) return console.log('FS ERROR', err);
+
+                        console.log('write to file');
+
+                        // send html
+                        res.send(200, minified);
+                    });
+                });
+            });
         });
     }
 
